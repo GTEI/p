2cc0dd68c616acbd70cf383fb586b32c9849f1ab
GTEI <112129525+GTEI@users.noreply.github.com>
2025-02-07 10:26:08 +0300

    Add files via upload

---
+++ pythonstartup.py
@@ -0,0 +1,11 @@
+[project]
+name = "python-template"
+version = "0.1.0"
+TelegramBotAPI = '2.26.0'
+description = ""
+authors = ["Your Name <you@example.com>"]
+requires-python = ">=3.11"
+dependencies = [
+    "pytelegrambotapi>=4.26.0",
+    "ratelimit>=2.2.1",
+]
